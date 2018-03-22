2340d30070682f4e82a589421affd88ac6b6f996
Seth-Rothschild <seth.rothschild@featurelabs.com>
2018-03-22 17:36:19 -0400

    Move load_data to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 from random import randint
 import pandas as pd
+
+def load_data(data_path):
+    operational_settings = ['operational_setting_{}'.format(i + 1) for i in range (3)]
+    sensor_columns = ['sensor_measurement_{}'.format(i + 1) for i in range(26)]
+    cols = ['engine_no', 'time_in_cycles'] + operational_settings + sensor_columns
+    data = pd.read_csv(data_path, sep=' ', header=-1, names=cols)
+    data = data.drop(cols[-5:], axis=1)
+    data['index'] = data.index
+    data.index = data['index']
+    data['time'] = pd.date_range('1/1/2000', periods=data.shape[0], freq='s')
+    return data
+
 def new_labels(data, labels):
     ct_ids = []
     ct_times = []
@@ -18,6 +30,7 @@ def new_labels(data, labels):
                        'RUL': ct_labels})
     ct = ct[['engine_no', 'cutoff_time', 'RUL']]
     ct.index = ct['engine_no']
+    ct.index = ct.index.rename('index')
     return ct
 
 def make_cutoff_times(data):
