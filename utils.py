6f690324940e9c0b6acedaab014dcf5fa129f5b3
Jeff Hernandez <inboxjeff@outlook.com>
2019-09-16 17:41:23 -0500

    working notebook

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def load_data(data_path):
     operational_settings = ['operational_setting_{}'.format(i + 1) for i in range (3)]
     sensor_columns = ['sensor_measurement_{}'.format(i + 1) for i in range(26)]
     cols = ['engine_no', 'time_in_cycles'] + operational_settings + sensor_columns
-    data = pd.read_csv(data_path, sep=' ', header=-1, names=cols)
+    data = pd.read_csv(data_path, sep=' ', header=None, names=cols)
     data = data.drop(cols[-5:], axis=1)
     data['index'] = data.index
     data.index = data['index']
